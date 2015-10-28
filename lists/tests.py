c7dd7dfa85e73c501a93fe80a299ac0fc255ba3a
bushig <bushig@mail.ru>
2015-10-29 00:16:02 +0300

    Refactor home page to use render

--- lists/tests.py
+++ lists/tests.py
@@ -2,15 +2,15 @@ from django.test import TestCase
 from django.core.urlresolvers import resolve
 from lists.views import home_page
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 class HomePageTests(TestCase):
     def test_main_page_url(self):
         found=resolve('/')
         self.assertEqual(found.func,home_page)
 
-    def test_main_page_title(self):
+    def test_main_page_returns_right_html(self):
         request=HttpRequest()
         response=home_page(request)
-        self.assertTrue(response.content.startswith(b'<html>'))
-        self.assertIn(b'<title>To-Do lists</title>',response.content)
-        self.assertTrue(response.content.endswith(b'</html>'))
+        expected_html=render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
--- lists/views.py
+++ lists/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http.response import HttpResponse
 
 def home_page(request):
-    return HttpResponse('<html><head><title>To-Do lists</title></head><h1>To-Do app</h1>qweqewqewwe</html>')
+    return render(request,'home.html')
