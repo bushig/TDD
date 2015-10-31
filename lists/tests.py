52e0ed1f75153643c8cc8b2e988587581e1a3ac0
bushig <bushig@mail.ru>
2015-11-01 01:43:35 +0300

    Now main page shows stuff

--- lists/tests.py
+++ lists/tests.py
@@ -14,3 +14,14 @@ class HomePageTests(TestCase):
         response=home_page(request)
         expected_html=render_to_string('home.html')
         self.assertEqual(response.content.decode(), expected_html)
+
+    def test_main_page_post_requests_working(self):
+        request=HttpRequest()
+        request.method='POST'
+        request.POST['item_text']='A new list item'
+
+        response=home_page(request)
+
+        self.assertIn('A new list item', response.content.decode())
+        expected_html=render_to_string('home.html', {'new_item_text':'A new list item'})
+        self.assertEqual(response.content.decode(), expected_html)
